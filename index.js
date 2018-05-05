86ba42ce0d604352f2b615dc35196dd0d3e5fb59
Dejan Zelic <dejanzelic@gmail.com>
2018-05-05 11:09:24 -0700

    initial

---
+++ index.js
@@ -0,0 +1,31 @@
+//Get time elements:
+els = document.getElementsByTagName("time")
+
+//get the links for each:
+for (var i = 0; i < els.length; i++) {
+    console.log(els[i].parentElement.href); 
+}
+
+
+
+
+var xhttp = new XMLHttpRequest();
+var req_variables = {
+	"id":"1989913378",
+	"first":"50"
+}
+var req_query_hash ="58712303d941c6855d4e888c5f0cd22f"
+
+url ='https://www.instagram.com/graphql/query/?query_hash=' + req_query_hash + '&variables=' + JSON.stringify(req_variables)
+xhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		console.log(this.responseText);
+	}
+};
+xhttp.open("GET", url, true);
+xhttp.send();
+
+//url: https://www.instagram.com/graphql/query/?query_hash=58712303d941c6855d4e888c5f0cd22f&variables={%22id%22:%221989913378%22,%22first%22:50}
+
+
+//url for next: https://www.instagram.com/graphql/query/?query_hash=58712303d941c6855d4e888c5f0cd22f&variables={%22id%22:%221989913378%22,%22first%22:50,%22after%22:%22AQC-2bE5-9-LexxEK5ooxTDsJwViC0Tc5X_imUcgnlY9cXPzeV9e_XfV5919cRfW0q9hrSoTrBEXS_mN4bfqre1_RSVT-vtjYJmuondO0lNPdg%22}
